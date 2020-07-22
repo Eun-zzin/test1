8e7e27ede1cb0d78100f558017f9d268b977535e
Eun-zzin <peunjin96@naver.com>
2020-07-22 13:38:55 +0900

    003.day

--- 061.py
+++ 061.py
@@ -36,32 +36,43 @@ class FASTQ:
     def __init__(self, file_name:str):
         self.file_name = file_name
         self.read_num = 0
+        self.base = {}
 
     def count_read_num(self):
         cnt = 0
         with open(self.file_name,'r') as handle:
             for line in handle:
-                if cnt % 4 ==0: #가장 첫줄
+                if cnt % 4 ==0: #가장 첫줄(header)
                     header = line.strip()
                     self.read_num +=1
-                elif cnt %4 == 1:
+                elif cnt %4 == 1: #두번째 줄(seq)
                     seq = line.strip()
-                elif cnt %4 ==3:
+                    for s in seq:
+                        if s in self.base:
+                            self.base[s] +=1
+                        else:
+                            self.base[s] =1
+                elif cnt %4 ==3: #세번째 줄(quality)
                     qual = line.strip()
                 cnt+=1
 
+    def count_base_num(self):
+        cnt = 0
+        cnt += self.base.values()
+        return cnt
 
 if __name__=="__main__":
     if len(sys.argv) != 2:
         print(f"#usage: python {sys.argv[0]} {fasta}")
         sys.exit()
     file_name = sys.argv[1]
-    t = FASTA(file_name) 
-    t.count_base() #count_base를 먼저 실행해야
-    print(t.count)
-    t.get_length() #length를 count할 수 있다. (count 변수가 필요하기 때문)
-    print(t.length)
+#    t = FASTA(file_name) 
+#    t.count_base() #count_base를 먼저 실행해야
+#    print(t.count)
+#    t.get_length() #length를 count할 수 있다. (count 변수가 필요하기 때문)
+#    print(t.length)
 
     q = FASTQ(file_name)
     q.count_read_num()
     print(q.read_num)
+    print(q.base)
--- kmer.py
+++ kmer.py
@@ -8,10 +8,12 @@ def Mer(base1,base2,n):
     base = [] #base 내에 합친것이 쌓이도록 빈 리스트 설정함
     if n == 1: # n값이 1로 돌아오면 base2,즉 base를 base2에 담아 출력하도록!
         return base2 
-    for i in base1:
-        for j in base2:        
-            base.append(i+j) #리스트 요소마다 더하고
-    return Mer(base1,base,n-1)    
+    for i in range(len(base1)):
+        for j in range(len(base2)):        
+            base.append(base1[i] + base2[j]) #리스트 요소마다 더하고
+    base2 = base #더한 리스트를 base2에 넣은 후 
+    for i in range(n): #n번째 돌린다.
+        return Mer(base1,base2,n-1) 
 #그리고 base를 넣은 base2와 기본(A,C,G,T)가 포함된 base1을 더하여 다시 함수를 돌린다. 따라서 base1 + base가 되도록!
 
 n = int(sys.argv[1])
